fab9f76043169265db7122fcb0a73583c9e38e79
Russell Steicke <russells@adelie.cx>
2014-02-17 20:00:12 +0800

    Make the auto time adjustment check better.
    When the script starts, the second minute it sees will be the base for
    the time offset.  24 hours later it will report on the difference in
    offsets.
    
    We use the second minute rather than the first minute, to try to get
    this measured with warm cache.
    
    Signed-off-by: Russell Steicke <russells@adelie.cx>

--- print-time-offsets.pl
+++ print-time-offsets.pl
@@ -16,33 +16,70 @@
 use POSIX qw( mktime strftime );
 use Time::HiRes qw( gettimeofday );
 
+my $minute_counter = -1;
+
+my $cal_minute;
+my $first_date;
+my $first_diff;
+
+
 while (<>) {
 
-    if (m/^TIME=([0-2][0-9])([0-5][0-9])$/) {
-	($t, $us) = gettimeofday();
-	print "t   =$t us=$us\n";
-	($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime($t);
-	$hc_hour = int $1;
-	$hc_minute = int $2;
+	if (m/^TIME=([0-2][0-9])([0-5][0-9])$/) {
 
-	# Now replace the HC hour and minute and zero seconds in the
-	# current time, and see what the result is.
-	$hc_t = mktime(0,$hc_minute,$hc_hour,$mday,$mon,$year);#,$wday,$yday,$isdst);
-	#print "hc_t=$hc_t\n";
+		$minute_counter += 1;
 
-	#print "Hour=$1\n";
-	#print "Minute=$2\n";
-	#print "hc_hour=$hc_hour\n";
-	#print "hc_minute=$hc_minute\n";
+		($t, $us) = gettimeofday();
+		print "t   =$t us=$us\n";
+		($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst)
+		    = localtime($t);
+		$hc_hour = int $1;
+		$hc_minute = int $2;
 
-	print sprintf("my time %ld %02d:%02d:%02d.%06d %04d/%02d/%02d\n",
-		      $t, $hour, $min, $sec, $us, $year+1900, $mon+1, $mday);
-	print sprintf("HC time %ld %02d:%02d:00", $hc_t, $hc_hour, $hc_minute);
+		# Now replace the HC hour and minute and zero seconds in the
+		# current time, and see what the result is.
+		$hc_t = mktime(0,$hc_minute,$hc_hour,$mday,$mon,$year);
+		#print "hc_t=$hc_t\n";
 
-	# Now find out the difference between the current time and what HC
-	# thinks is the current time.
-	$diff = $hc_t - ($t + ($us/1e6));
-	print sprintf("                    %0.6f\n", $diff);
+		#print "Hour=$1\n";
+		#print "Minute=$2\n";
+		#print "hc_hour=$hc_hour\n";
+		#print "hc_minute=$hc_minute\n";
 
-    }
+		print sprintf("my time %ld %02d:%02d:%02d.%06d %04d/%02d/%02d\n",
+			      $t, $hour, $min, $sec, $us,
+			      $year+1900, $mon+1, $mday);
+		print sprintf("HC time %ld %02d:%02d:00",
+			      $hc_t, $hc_hour, $hc_minute);
+
+		# Now find out the difference between the current time and what HC
+		# thinks is the current time.
+		$diff = $hc_t - ($t + ($us/1e6));
+		print sprintf("                    %0.6f\n", $diff);
+
+		if ($minute_counter == 1) {
+			$cal_minute = "$1$2";
+			$first_diff = $diff;
+			$first_date = sprintf("%04d/%02d/%02d %02d:%02d",
+					      $year+1900, $mon+1, $mday,
+	 				      $hour, $min);
+			print "\n\n----------------\n";
+			print sprintf("Start: $first_date %10.6f\n", $first_diff);
+			print "----------------\n\n";
+		} else {
+			if ("$1$2" eq $cal_minute) {
+				$now_date = sprintf("%04d/%02d/%02d %02d:%02d",
+						    $year+1900, $mon+1, $mday, $hour, $min);
+				print "\n\n----------------\n";
+				print sprintf("Start: $first_date %10.6f\n", $first_diff);
+				print sprintf("Now  : $now_date %10.6f\n", $diff);
+				print "----------------\n\n";
+			}
+		}
+
+	}
 }
+
+# Local variables:
+# perl-indent-level:8
+# End:
